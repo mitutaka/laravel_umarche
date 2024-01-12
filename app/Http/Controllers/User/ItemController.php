e399def7819310ff8cd74ce03aba340fc4127b87
hanada <90606895+mitutaka@users.noreply.github.com>
2024-01-12 21:31:18 +0900

    表示件数

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Stock;
+use App\Models\PrimaryCategory;
 use Illuminate\Support\Facades\DB;
 
 class ItemController extends Controller
@@ -27,11 +28,23 @@ class ItemController extends Controller
     });
   }
 
-  public function index()
+  public function index(Request $request)
   {
-    $products = Product::availableItems()->get();
+    // dd($request);
 
-    return view('user.index', compact('products'));
+    $categories = PrimaryCategory::with('secondary')
+      ->get();
+
+    $products = Product::availableItems()
+      ->selectCategory($request->category ?? '0')
+      ->searchKeyword($request->keyword)
+      ->sortOrder($request->sort)
+      ->paginate($request->pagination ?? '20');
+
+    return view(
+      'user.index',
+      compact('products', 'categories')
+    );
   }
 
   public function show($id)
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -66,10 +66,7 @@ class Product extends Model
 
   public function users()
   {
-    return $this->belongsToMany(
-      User::class,
-      'carts'
-    )
+    return $this->belongsToMany(User::class, 'carts')
       ->withPivot(['id', 'quantity']);
   }
 
@@ -81,19 +78,14 @@ class Product extends Model
         DB::raw('sum(quantity) as quantity')
       )
       ->groupBy('product_id')
-      ->having('quantity', '>=', 1);
+      ->having('quantity', '>', 1);
 
     return $query
       ->joinSub($stocks, 'stock', function ($join) {
         $join->on('products.id', '=', 'stock.product_id');
       })
       ->join('shops', 'products.shop_id', '=', 'shops.id')
-      ->join(
-        'secondary_categories',
-        'products.secondary_category_id',
-        '=',
-        'secondary_categories.id'
-      )
+      ->join('secondary_categories', 'products.secondary_category_id', '=', 'secondary_categories.id')
       ->join('images as image1', 'products.image1', '=', 'image1.id')
       ->where('shops.is_selling', true)
       ->where('products.is_selling', true)
@@ -106,5 +98,53 @@ class Product extends Model
         'secondary_categories.name as category',
         'image1.filename as filename'
       );
+  }
+
+  public function scopeSortOrder($query, $sortOrder)
+  {
+    if ($sortOrder === null || $sortOrder === \Constant::SORT_ORDER['recommend']) {
+      return $query->orderBy('sort_order', 'asc');
+    }
+    if ($sortOrder === \Constant::SORT_ORDER['higherPrice']) {
+      return $query->orderBy('price', 'desc');
+    }
+    if ($sortOrder === \Constant::SORT_ORDER['lowerPrice']) {
+      return $query->orderBy('price', 'asc');
+    }
+    if ($sortOrder === \Constant::SORT_ORDER['later']) {
+      return $query->orderBy('products.created_at', 'desc');
+    }
+    if ($sortOrder === \Constant::SORT_ORDER['older']) {
+      return $query->orderBy('products.created_at', 'asc');
+    }
+  }
+
+  public function scopeSelectCategory($query, $categoryId)
+  {
+    if ($categoryId !== '0') {
+      return $query->where('secondary_category_id', $categoryId);
+    } else {
+      return;
+    }
+  }
+
+  public function scopeSearchKeyword($query, $keyword)
+  {
+    if (!is_null($keyword)) {
+      //全角スペースを半角に
+      $spaceConvert = mb_convert_kana($keyword, 's');
+
+      //空白で区切る
+      $keywords = preg_split('/[\s]+/', $spaceConvert, -1, PREG_SPLIT_NO_EMPTY);
+
+      //単語をループで回す
+      foreach ($keywords as $word) {
+        $query->where('products.name', 'like', '%' . $word . '%');
+      }
+
+      return $query;
+    } else {
+      return;
+    }
   }
 }
